7a17fa69fb0525cec69eae0c43ca0ab4a33a39ba
helloteam-developer1 <68662199+helloteam-developer1@users.noreply.github.com>
2021-08-19 15:01:30 -0500

    MI PRIMER CAMBIO

--- src/Pagina/Header.js
+++ src/Pagina/Header.js
@@ -5,9 +5,10 @@ class Header extends React.Component {
     render() {
         return(
             <>
-            <h1>TUYO MAU</h1>
+            <h1>TUYO EDWIN</h1>
             </>
         )
     }
 }
+
 export default Header;
