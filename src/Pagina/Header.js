df29a4e339ade14fb2ead13fc7237a8ae5c8a4da
helloteam-developer1 <68662199+helloteam-developer1@users.noreply.github.com>
2021-08-19 15:26:08 -0500

    segundo cambio

--- src/Pagina/Header.js
+++ src/Pagina/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return(
             <>
-            <h1>TUYO 2DWIN</h1>
+            <h1>Hola mundo</h1>
             </>
         )
     }
