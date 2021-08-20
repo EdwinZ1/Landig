b7debcf29bc8ac7b5393848bf6230257b56fe6ce
helloteam-developer1 <68662199+helloteam-developer1@users.noreply.github.com>
2021-08-20 15:03:51 -0500

    primer cambio menú

--- src/Pagina/Header.js
+++ src/Pagina/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Nav,NavItem,NavLink,Button} from 'reactstrap';
+import '../App.css'
 
 class Header extends React.Component {
     render() {
         return(
             <>
-            <h1>Hola mundo cambio dos</h1>
+            <div>
+      
+<br/>
+      <br/>
+    
+           <Nav  className="Estilos-menu Estilos-font">
+        <NavLink href="#" className="color1">¿QUÉ ES HELLOSURVEY?</NavLink> <NavLink href="#" className="color1">¿CÚAL ES LA POTENCIA DE HELLOSURVEY?</NavLink> <NavLink href="#" className="color1">¿QUE PUEDO HACER CON HELLOSURVEY?</NavLink> <NavLink className="color1" href="#">CONTACTO</NavLink>   <Button className=" Bgrcolor2" >Prueba  HELLO SURVEY </Button>
+      </Nav>
+    </div>
             </>
         )
     }
