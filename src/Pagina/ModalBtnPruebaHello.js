5e957d97240a6135ee0b4e91430cb45eceb60056
helloteam-developer1 <68662199+helloteam-developer1@users.noreply.github.com>
2021-10-08 11:14:16 -0500

    primercam081021

--- src/Pagina/ModalBtnPruebaHello.js
+++ src/Pagina/ModalBtnPruebaHello.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
-import { Modal, ModalHeader, ModalFooter, ModalBody, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink ,Button } from 'reactstrap';
-
+import { Modal, ModalHeader, ModalFooter, ModalBody, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink ,Button, Row } from 'reactstrap';
+import M1 from '../IMG/14_Foother/Whats_Icon.png';
+import M2 from '../IMG/14_Foother/Mail_Icon1.png';
 export default class ModalBtnPruebaHello extends React.Component {
   
 
@@ -32,18 +33,62 @@ render() {
 
 {/*/////////////////////////// Modal del BOTÓN ////////////////////////// */}
         <div>
+        
         <Modal isOpen={this.state.abrir} toggle={()=>this.setState({abrir:false})} >
-        <ModalHeader>Modal title</ModalHeader>
+  
+ 
+        <ModalHeader   style={{background: '#FACC2E', border:'0' }} cssModule={{ 'modal-title': 'w-100 text-right' }}    onClick={() => this.setState({abrir: !this.state.abrir})}>  
+      
+        <button  style={{background: '#FACC2E'}} type="button" class="close" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+      
+{/*       
+        <h1  style={{color: 'white',fontSize:"100%"}}>Prueba Hello Survey
+
+        
+                            </h1>          */}
+        </ModalHeader>
+       
+      
         <ModalBody>
-          <b>Look at the top right of the page/viewport!</b><br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <center>
+        <h1>¿Qué esperas?</h1>
+        
+         </center><br/><br/>
+         <div >
+     
+          <p>Solicita una prueba de Hello Survey ya ,es muy fácil.<br/> Contáctanos por WhatsApp haciendo clic en el<br/> siguiente enlace:</p><br/>
+         
+          </div>
+          <div>
+             
+          <img src={M1}  alt=""  width="20px"/> 
+          &nbsp;&nbsp;
+              WhatsApp<br/><br/>
+              O envíanos un correo electrónico a:<br/><br/>
+              <img src={M2}  width="20px" />
+               &nbsp;&nbsp;
+              julio.ferrer@hellomexico.mx<br/><br/>
+           
+              <h4><strong>¡Solicita la prueba ya!</strong></h4>
+             
+          </div>
+
         </ModalBody>
+        <hr/>
+             
         <div onClick={() => this.setState({ abrir: !this.state.abrir })}>
-                <Button style={{ color: 'transparent', background: '#7D5C00' }} className="btn btn-sm bp">
-                  <b style={{ color: 'white' }}>&nbsp;&nbsp;CERRAR&nbsp;&nbsp;</b>
-                  </Button>
+                             
+          <button className="btn col-2" type="submit" style={{ backgroundColor: 'black', color: 'white', position: 'absolute', marginLeft: '79%', background: '  #ECC538 '}}  >Cerrar</button>
+
+
+
+                  <br/> <br/>
                   </div>
       </Modal>
+
+     
         </div>
         
 
