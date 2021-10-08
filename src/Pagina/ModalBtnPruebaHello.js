d7749bc2c87f7cc6fdc955671870a1b7c56070d6
helloteam-developer1 <68662199+helloteam-developer1@users.noreply.github.com>
2021-10-08 13:38:47 -0500

    segundoCam081021

--- src/Pagina/ModalBtnPruebaHello.js
+++ src/Pagina/ModalBtnPruebaHello.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Modal, ModalHeader, ModalFooter, ModalBody, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink ,Button, Row } from 'reactstrap';
 import M1 from '../IMG/14_Foother/Whats_Icon.png';
 import M2 from '../IMG/14_Foother/Mail_Icon1.png';
+import '../Style/Generales.css';
 export default class ModalBtnPruebaHello extends React.Component {
   
 
@@ -31,28 +32,19 @@ render() {
 
 
 
-{/*/////////////////////////// Modal del BOTÓN ////////////////////////// */}
-        <div>
-        
-        <Modal isOpen={this.state.abrir} toggle={()=>this.setState({abrir:false})} >
-  
- 
-        <ModalHeader   style={{background: '#FACC2E', border:'0' }} cssModule={{ 'modal-title': 'w-100 text-right' }}    onClick={() => this.setState({abrir: !this.state.abrir})}>  
-      
-        <button  style={{background: '#FACC2E'}} type="button" class="close" aria-label="Close">
+            <Modal isOpen={this.state.abrir} size="m" >
+                    <ModalHeader cssModule={{ 'modal-title': 'w-100 text-right' }} style={{ background: '#F8C837' }}>
+                    <Button  style={{float:'right',  background: '#F8C837', color: "#ffffff", borderColor:'#F8C837'}}  type="button" class="close"  onClick={() => this.setState({abrir: !this.state.abrir})}>
                                 <span aria-hidden="true">&times;</span>
-                            </button>
-      
-{/*       
-        <h1  style={{color: 'white',fontSize:"100%"}}>Prueba Hello Survey
+                            </Button>
+                            <br />
+                            <h1 className="text-center"   style={{color: 'white',fontSize:"100%" }}>Prueba Hello Survey</h1>          
+                   
 
-        
-                            </h1>          */}
-        </ModalHeader>
-       
-      
-        <ModalBody>
-          <center>
+                    </ModalHeader>
+                    <ModalBody>
+
+                    <center>
         <h1>¿Qué esperas?</h1>
         
          </center><br/><br/>
@@ -61,7 +53,7 @@ render() {
           <p>Solicita una prueba de Hello Survey ya ,es muy fácil.<br/> Contáctanos por WhatsApp haciendo clic en el<br/> siguiente enlace:</p><br/>
          
           </div>
-          <div>
+          <div style={{textAlign:'center', fontSize:"20px"}}>
              
           <img src={M1}  alt=""  width="20px"/> 
           &nbsp;&nbsp;
@@ -80,17 +72,15 @@ render() {
              
         <div onClick={() => this.setState({ abrir: !this.state.abrir })}>
                              
-          <button className="btn col-2" type="submit" style={{ backgroundColor: 'black', color: 'white', position: 'absolute', marginLeft: '79%', background: '  #ECC538 '}}  >Cerrar</button>
+          <button className="btn col-2" type="submit" style={{ backgroundColor: 'black', color: 'white', position: 'absolute', marginLeft: '79%', background: '#F8C837'}}  >Cerrar</button>
 
 
 
                   <br/> <br/>
                   </div>
-      </Modal>
-
-     
-        </div>
-        
+                   
+                   
+                </Modal>
 
 
 
